47b8e843c3731011f4d4d28e998dc825ea41f691
heni <eugene.krokhalev@gmail.com>
2013-04-05 09:29:47 +0400

    small refactoring - extract common part of statistics classes into base class

--- stattables.h
+++ stattables.h
@@ -5,23 +5,27 @@
 #include <algorithm>
 
 
-class UnigramBlocksStatistics {
-    static const size_t CELL_SIZE = 256;
+template <typename Derived, size_t CELL_SIZE>
+class StatisticsBase {
+protected:
+    static const size_t CellSize = CELL_SIZE;
     const size_t ChunkSize;
     size_t AggregatedBlocksCount;
     std::vector<std::vector<size_t>> Internal;
 public:
-    UnigramBlocksStatistics(size_t chunksize)
+    StatisticsBase(size_t chunksize)
         : ChunkSize(chunksize)
         , AggregatedBlocksCount(0)
         , Internal(ChunkSize, std::vector<size_t>(CELL_SIZE, 0))
     {}
 
-    void Update(const std::vector<unsigned char>& buffer) {
-        assert(buffer.size() == ChunkSize);
-        for (size_t i = 0; i < ChunkSize; ++i)
-            ++Internal[i][buffer[i]];
-        ++AggregatedBlocksCount;
+    void Dump(std::ostream& out) const {
+        for (const auto& subStat: Internal) {
+            assert(accumulate(subStat.begin(), subStat.end(), 0ull) == AggregatedBlocksCount);
+            out.write(reinterpret_cast<const char*>(&*subStat.begin()), CELL_SIZE * sizeof(subStat[0]));
+            if (!out)
+                throw std::runtime_error("statics dump error");
+        }
     }
 
     std::vector<double> GetSlice(size_t index) const {
@@ -32,18 +36,9 @@ public:
         return retval;
     }
 
-    void Dump(std::ostream& out) const {
-        for (const auto& subStat: Internal) {
-            assert(accumulate(subStat.begin(), subStat.end(), 0ull) == AggregatedBlocksCount);
-            out.write(reinterpret_cast<const char*>(&subStat[0]), CELL_SIZE * sizeof(subStat[0]));
-            if (!out)
-                throw std::runtime_error("statistics dump error");
-        }
-    }
-
     void Load(std::istream& in) {
         for (auto& subStat: Internal) {
-            in.read(reinterpret_cast<char*>(&subStat[0]), CELL_SIZE * sizeof(subStat[0]));
+            in.read(reinterpret_cast<char*>(&*subStat.begin()), CELL_SIZE * sizeof(subStat[0]));
             if (!in)
                 throw std::runtime_error("statistics load error");
             if (&subStat == &*Internal.begin())
@@ -54,16 +49,29 @@ public:
     }
 };
 
-class BigramBlocksStatistics {
-    static const size_t CELL_SIZE = 256 * 256;
-    const size_t ChunkSize;
-    size_t AggregatedBlocksCount;
-    std::vector<std::vector<size_t>> Internal;
+
+class UnigramBlocksStatistics : public StatisticsBase<UnigramBlocksStatistics, 256> {
+    typedef StatisticsBase<UnigramBlocksStatistics, 256> TBase;
+    
+public:
+    UnigramBlocksStatistics(size_t chunksize)
+        : TBase(chunksize)
+    {}
+
+    void Update(const std::vector<unsigned char>& buffer) {
+        assert(buffer.size() == ChunkSize);
+        for (size_t i = 0; i < ChunkSize; ++i)
+            ++Internal[i][buffer[i]];
+        ++AggregatedBlocksCount;
+    }
+};
+
+
+class BigramBlocksStatistics : public StatisticsBase<BigramBlocksStatistics, 256 * 256> {
+    typedef StatisticsBase<BigramBlocksStatistics, 256 * 256> TBase;
 public:
     BigramBlocksStatistics(size_t chunksize)
-        : ChunkSize(chunksize)
-        , AggregatedBlocksCount(0)
-        , Internal(ChunkSize - 1, std::vector<size_t>(CELL_SIZE, 0))
+        : TBase(chunksize)
     {}
 
     void Update(const std::vector<unsigned char>& buffer) {
@@ -73,34 +81,5 @@ public:
             ++Internal[i][offset];
         }
         ++AggregatedBlocksCount;
-    }
-
-    std::vector<double> GetSlice(size_t index) const {
-        std::vector<double> retval(CELL_SIZE);
-        const double expected = AggregatedBlocksCount * 1.0;
-        for (size_t i = 0; i < CELL_SIZE; ++i)
-            retval[i] = Internal[index][i] / expected;
-        return retval;
-    }
-
-    void Dump(std::ostream& out) const {
-        for (const auto& subStat: Internal) {
-            assert(accumulate(subStat.begin(), subStat.end(), 0ull) == AggregatedBlocksCount);
-            out.write(reinterpret_cast<const char*>(&subStat[0]), CELL_SIZE * sizeof(subStat[0]));
-            if (!out)
-                throw std::runtime_error("statistics dump error");
-        }
-    }
-
-    void Load(std::istream& in) {
-        for (auto& subStat: Internal) {
-            in.read(reinterpret_cast<char*>(&subStat[0]), CELL_SIZE * sizeof(subStat[0]));
-            if (!in)
-                throw std::runtime_error("statistics load error");
-            if (&subStat == &*Internal.begin())
-                AggregatedBlocksCount = accumulate(subStat.begin(), subStat.end(), 0ull);
-            else
-                assert(accumulate(subStat.begin(), subStat.end(), 0ull) == AggregatedBlocksCount);
-        }
     }
 };
